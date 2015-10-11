d26e85e5f564f60a4f60cd9f459dfc5385564e4a
rej017 <regz_jane01@yahoo.com>
2015-10-11 10:45:38 +0800

    sql-query

--- includes/data-processors/processJOajax.php
+++ includes/data-processors/processJOajax.php
@@ -3,12 +3,12 @@
     // Job Order Data
     $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    
+
 	$_SESSION['joNumber'] = $_POST['selectedID'];
 	$num = $_SESSION['joNumber'];
 
     $sql="SELECT * from servicelogs join services using (serviceid) join joborders 
-        using (joborderid) join clients using (clientid) where joborderid = 1;";
+        using (joborderid) join clients using (clientid) where joborderid = $num";
     $stmt=$conn->prepare($sql);
     $stmt->execute();
     $results_array=$stmt->fetchAll(PDO::FETCH_ASSOC);
