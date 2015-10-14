f75e01b67f98f99a595eec41dd9d4ff7253cee7d
rej017 <regz_jane01@yahoo.com>
2015-10-14 14:03:18 +0800

    removed refresh button

--- js/jquery.bootgrid.js
+++ js/jquery.bootgrid.js
@@ -342,20 +342,20 @@
                     actions = $(tpl.actions.resolve(getParams.call(this)));
 
                 // Refresh Button
-                if (this.options.ajax)
-                {
-                    var refreshIcon = tpl.icon.resolve(getParams.call(this, { iconCss: css.iconRefresh })),
-                        refresh = $(tpl.actionButton.resolve(getParams.call(this,
-                        { content: refreshIcon, text: this.options.labels.refresh })))
-                            .on("click" + namespace, function (e)
-                            {
-                                // todo: prevent multiple fast clicks (fast click detection)
-                                e.stopPropagation();
-                                that.current = 1;
-                                loadData.call(that);
-                            });
-                    actions.append(refresh);
-                }
+                // if (this.options.ajax)
+                // {
+                //     var refreshIcon = tpl.icon.resolve(getParams.call(this, { iconCss: css.iconRefresh })),
+                //         refresh = $(tpl.actionButton.resolve(getParams.call(this,
+                //         { content: refreshIcon, text: this.options.labels.refresh })))
+                //             .on("click" + namespace, function (e)
+                //             {
+                //                 // todo: prevent multiple fast clicks (fast click detection)
+                //                 e.stopPropagation();
+                //                 that.current = 1;
+                //                 loadData.call(that);
+                //             });
+                //     actions.append(refresh);
+                // }
 
                 // Row count selection
                 renderRowCountSelection.call(this, actions);
