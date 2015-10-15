591077fc59053debea6672eb039e1f38d4c8747f
trifrancisco <trifrancisco@users.noreply.github.com>
2015-10-15 21:33:05 +0800

    ~modified services

--- services.php
+++ services.php
@@ -15,47 +15,31 @@
     <br />
 <!-- Page Content -->
     <div class="container">
-        <div class="row">
-            <div class="jumbotron" style="height: 100px;">
-                <h2 style="margin-top: -15px; margin-left: -60px; text-align: center;">Services</h2>
-            </div>
-            
+        <div class="jumbotron" style="height: 100px;">
+            <h2 style="margin-top: -15px; margin-left: -60px; text-align: center;">Services</h2>
         </div>
-        <div id="myTabContent" class="tab-content">
-            <div class="row tab-pane fade active in" id="joTab">
-                <div class="col-lg-12">
-                    <br>
-                </div>
-                <div class="col-md-12">       
-                        <!-- Job Order Buttons -->
-                        <div class="actionBtns">
-                            <button type="button" id="newservicebtn" class="btn btn-info" data-toggle="modal" 
-                                href="#addServiceModal"><i class="fa fa-plus fa-fw"></i> New Service </button>
-                            <button type="button" id="editservicebtn" class="btn btn-info" data-toggle="modal">
-                                <i class="fa fa-pencil-square-o fa-fw"></i> Edit/Update </button>
-                            <button type="button" id="deleteservicebtn" class="btn btn-info" data-toggle="modal">
-                                <span class="glyphicon glyphicon-remove-sign"></span> </button>
-                        </div>
-                        <!-- Job Order Table -->
-                        <table  id="serviceTable" class="table table-condensed table-hover">
-                            <thead>
-                                <tr>
-                                    <th data-column-id="serviceid" data-visible="false" data-identifier="true">
-                                        Service ID</th>
-                                    <th data-column-id="servicename">
-                                        Service name</th>
-                                    <th data-column-id="serviceprice">
-                                        Price</th>
-                                </tr>
-                            </thead>  
-                        </table>
-                        <hr>
-                    
-                </div>
-            </div>
-        
-            
+        <!-- Service Buttons -->
+        <div class="actionBtns">
+            <button type="button" id="newservicebtn" class="btn btn-info" data-toggle="modal" 
+                href="#addServiceModal"><i class="fa fa-plus fa-fw"></i> New Service </button>
+            <button type="button" id="editservicebtn" class="btn btn-info" data-toggle="modal">
+                <i class="fa fa-pencil-square-o fa-fw"></i> Edit/Update </button>
+            <button type="button" id="deleteservicebtn" class="btn btn-info" data-toggle="modal">
+                <span class="glyphicon glyphicon-remove-sign"></span> </button>
         </div>
+        <!-- Service Table -->
+        <table  id="serviceTable" class="table table-condensed table-hover">
+            <thead>
+                <tr>
+                    <th data-column-id="serviceid" data-visible="false" data-identifier="true">
+                        Service ID</th>
+                    <th data-column-id="servicename">
+                        Service name</th>
+                    <th data-column-id="serviceprice">
+                        Price</th>
+                </tr>
+            </thead>  
+        </table>
         <hr>
         <?php include 'includes/footer.php'; ?>
     </div>
