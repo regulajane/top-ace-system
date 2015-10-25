9bcb2adc5f8dbed9b825fe7ef2a87160958c8622
carolino024 <carolinokevin@gmail.com>
2015-10-25 14:49:48 +0800

    - Added logs functionality

--- includes/data-processors/addSupply.php
+++ includes/data-processors/addSupply.php
@@ -12,33 +12,37 @@
 		$quantity = $_POST["inventQtyAdded"];
 
 		//Ingoing
-		/*
 		ini_set('date.timezone', 'Asia/Manila');
 		$isDate = date("Y-m-d"); 
 		$isTime = date("H:i:s");
 		$isQty = $quantity;
 		$enteredBy = $_SESSION["username"];
-		*/
+
+		echo "<script>alert('$isDate $isTime $isQty $enteredBy $inventID')</script>";
 		//-----------------------------UPDATE INVENTORY------------------------------------
 		$sql = 	"UPDATE inventory
 					SET inventoryquantity = inventoryquantity + '$quantity'
 					WHERE inventoryid = '$inventID'";
 		$stmt = $conn->prepare($sql);
 		$stmt->execute(); 
-		//-----------------------------INSERT INTO INGOINGSUPPLIES------------------------------------
-		/* $sql2 = "INSERT INTO ingoingsupplies (isDate, isTime, isQty, enteredBy, inventID) 
+
+		//-----------------------------INSERT INTO INGOING ITEMS------------------------------------
+		$sql2 = "INSERT INTO ingoingitems (isdate, time, quantity, enteredby, inventoryid) 
 					VALUES (?, ?, ?, ?, ?)";
-		$stmt2 = $conn->prepare($sql2);\
+
+		$stmt2 = $conn->prepare($sql2);
 
 		// Bind
-		$stmt2->bind_param("ssiss", $isDate, $isTime, $isQty, $enteredBy, $inventID);
+		$stmt2->bind_param("sssss", $isDate, $isTime, $isQty, $enteredBy, $inventID);
 		// Execute
 
-		$stmt2->execute(); */
+		$stmt2->execute(); 
+
 		//-----------------------------DROP addSupply TRIGGER------------------------------------
 		/*$sqlDropTrigger = "DROP TRIGGER addSupply";
 		mysqli_query($conn,$sqlDropTrigger);
 		*/
+
 		// Redirect
 		header('location:../../inventory.php');           			   
 	}
