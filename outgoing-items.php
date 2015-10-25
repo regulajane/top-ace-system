0a918684e6bb6052f73e6cc13a148a67584c305e
carolino024 <carolinokevin@gmail.com>
2015-10-25 14:51:17 +0800

    - Added logs functionality

--- outgoing-items.php
+++ outgoing-items.php
@@ -2,33 +2,36 @@
 <html lang="en">
 <head>
     <?php   include 'includes/header.php';
-            include 'includes/head-elements.php';   ?>
-    <title>Outgoing Supplies</title>
+            include 'includes/head-elements.php';   
+    if(!isset($_SESSION["username"])) {
+                header('Location: index.php?loggedout=true');}
+    ?>
+    <script src="js/outgoingitems-script.js"></script>
+    <title>Procured Items</title>
 </head>
 <body>
-    <?php include 'includes/nav.php'; ?>
-    <div class="container">          
-        <h3>Outgoing Supplies</h3>
-        <!-- Outgoing Supplies Buttons -->
-        <div class="actionBtns" style="position: relative; float:right;">
+   <?php include 'includes/nav.php'; ?>
+    <div class="container">        
+        <h3>Procured Items</h3>
+
+        <div class="actionBtns" style="position: relative; float:right; ">
             <div style="position: relative; margin-top: -40px;">
-                <a type="button" class="btn btn-primary" href="supplies.php" title="Supplies">
+                <a type="button" class="btn btn-primary" href="inventory.php" title="Supplies">
                     <i class="fa fa-arrow-left fa-fw"></i> Back</a>
-                <a type="button" class="btn btn-primary" href="includes/printOutgoingSupplies.php" title="Print Outgoing Supplies" 
+                <a type="button" class="btn btn-primary" href="includes/printIngoingSupplies.php" title="Print Ingoing Supplies" 
                     target="_blank"><i class="fa fa-print fa-fw"></i> Print</a>
             </div>
         </div>
-        <hr>
-        <!-- Outgoing Supplies Table -->
-        <table  id="outSuppliesTable" class="table table-condensed table-hover">
+        <hr style="margin-bottom: 70px">
+
+        <table  id="outItemsTable" class="table table-condensed table-hover">
             <thead>
                 <tr>
-                    <th data-column-id="" data-identifier="true" > Information coming soon </th>
-                    <th data-column-id="" > Information coming soon </th>
-                    <th data-column-id="" > Information coming soon </th>
-                    <th data-column-id="" > Information coming soon </th>
-                    <th data-column-id="" > Information coming soon </th>
-                    <th data-column-id="">  Information coming soon </th>
+                    <th data-column-id="isdate">Date</th>
+                    <th data-column-id="time">Time</th>
+                    <th data-column-id="quantity">Quantity Procured</th>
+                    <th data-column-id="enteredby">User</th>
+                    <th data-column-id="inventoryname">Item Name</th>
                 </tr>
             </thead>  
         </table>
