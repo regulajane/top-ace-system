27cda3e3d5edc2c91f58ee9ae7d66742897b3a61
carolino024 <carolinokevin@gmail.com>
2015-10-25 14:50:00 +0800

    - Added logs functionality

--- includes/data-processors/procureSupply.php
+++ includes/data-processors/procureSupply.php
@@ -12,13 +12,13 @@
 		$quantity = $_POST["inventQtyProcured"];
 
 		//outgoing
-		/*
+		
 		ini_set('date.timezone', 'Asia/Manila');
-		$osDate = date("Y-m-d"); 
-		$osTime = date("H:i:s");
-		$osQty = $quantity;
+		$otDate = date("Y-m-d"); 
+		$otTime = date("H:i:s");
+		$otQty = $quantity;
 		$procuredBy = $_SESSION["username"];
-		*/
+		
 		$sqlqty = "SELECT inventoryquantity, reorderlevel FROM inventory WHERE inventoryid = '" .$inventID. "'";
 		$result = $conn->query($sqlqty);
 		$resultRow = $result->fetch_assoc();
@@ -52,17 +52,17 @@
 			$stmt->execute();
 			
 			
-			//-----------------------------INSERT INTO INGOINGSUPPLIES------------------------------------
-			/*$sql2 = "INSERT INTO outgoingsupplies (osDate, osTime, osQty, procuredBy, supplyID) 
-						VALUES (?, ?, ?, ?, ?)";
-			$stmt2 = $conn->prepare($sql2);
-			// Bind
-			$stmt2->bind_param("ssiss", $osDate, $osTime, $osQty, $procuredBy, $supplyID);
-			// Execute
-			$stmt2->execute();*/
-			//-----------------------------DROP procureSupply TRIGGER------------------------------------
-			//$sqlDropTrigger = "DROP TRIGGER procureSupply";
-			//mysqli_query($conn,$sqlDropTrigger);
+			//-----------------------------INSERT INTO OUTGOINGSUPPLIES------------------------------------
+		$sql2 = "INSERT INTO outgoingitems (isdate, time, quantity, enteredby, inventoryid) 
+					VALUES (?, ?, ?, ?, ?)";
+
+		$stmt2 = $conn->prepare($sql2);
+
+		// Bind
+		$stmt2->bind_param("sssss", $otDate, $otTime, $otQty, $procuredBy, $inventID);
+		// Execute
+
+		$stmt2->execute(); 
 
 			
 			// Redirect
