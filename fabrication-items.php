b6bed4fa5e0dab02fa49dbdc71982e79eb578908
rej017 <regz_jane01@yahoo.com>
2015-11-23 02:51:41 +0800

    - modified page layout

--- fabrication-items.php
+++ fabrication-items.php
@@ -12,50 +12,45 @@
 	</head>
 	<body>
 		<?php include 'includes/nav.php'; ?>
-		<div id="fabrication">
-			<div class="container" style="margin-top: 20px">
+	    <div class="pagecontainer">
+	    <br>
+		    <div id="fabrication">
+		    	<div class="container">        
+		        <h4>Fabrication Items</h4>
+		        <div class="actionBtns" style="position: relative; float:right; ">
+		            <div style="position: relative; margin-top: -40px;">
+		                <a type="button" class="btn btn-primary" href="inventory.php" title="Inventory">
+		                    <i class="fa fa-arrow-left fa-fw"></i> Back</a>
+		            </div>
+		        </div>
+		        <hr>
 				<div class="actionBtns">
-							<a type="button" class="btn btn-info" href="inventory.php" >
-                    			<i class="fa fa-arrow-left fa-fw"></i> Back
-                    		</a>
-							<button type="button" id="addnew" class="btn btn-info" data-toggle="modal" 
-								href="#"><i class="fa fa-plus fa-fw"></i>Add New Item</button>
+					<button type="button" id="addnew" class="btn btn-info" data-toggle="modal" 
+						href="#"><i class="fa fa-plus fa-fw"></i>New Item</button>
 
-							<div class="btn-group">
-				                <a class="btn btn-info" href="#"><i class="fa fa-folder-open"></i> Upload Files </a>
-				                <a class="btn btn-info dropdown-toggle" data-toggle="dropdown" href="#">
-				                <span class="fa fa-caret-down"></span></a>
-				                <ul class="dropdown-menu">
-				                    <li>
-				                    	<a type="button" class="btn btn-info" data-toggle="modal"  href="#importNewItemsFabrication" 
-				                        style="text-align:left;  margin: 0px 5px 5px 5px; width: 94%; color: #FFF">
-				                        <i class="fa fa-list"></i> New Fabrication Items (.excel)
-				                        </a>
-				                    </li>
-				                </ul>
-		            		</div>
+					<div class="btn-group">
+		                <a class="btn btn-info" href="#"><i class="fa fa-folder-open"></i> Upload Files </a>
+		                <a class="btn btn-info dropdown-toggle" data-toggle="dropdown" href="#">
+		                <span class="fa fa-caret-down"></span></a>
+		                <ul class="dropdown-menu">
+		                    <li><a data-toggle="modal"  href="#importNewItemsFabrication">
+			                    	<i class="fa fa-list"></i> New Items (.excel)</a></li>
+		                </ul>
+	        		</div>
 				</div>
+				
 				<table  id="fabricationTable" class="table table-condensed table-hover table-striped" style="margin-top: 20px">
-				<thead>
-					<tr>
-						<th data-column-id="itemid" data-visible="false"  data-identifier="true">
-							Item ID
-						</th>
-						<th data-column-id="itemname">
-							Name
-						</th>
-						<th data-column-id="itemsizediam">
-							Size (diam)
-						</th>
-						<th data-column-id="itemsizelength">
-							Length (length)
-						</th>
-
-					</tr>
-				</thead>  
-			</table>
+					<thead>
+						<tr><th data-column-id="itemid" data-visible="false"  data-identifier="true">Item ID</th>
+							<th data-column-id="itemname">Name</th>
+							<th data-column-id="itemsizediam">Size (diam)</th>
+							<th data-column-id="itemsizelength">Length (length)</th>
+						</tr>
+					</thead>  
+				</table>
 				<?php include 'includes/modals/modal-fabricationitems.php';?>  
 				<?php include 'includes/footer.php'; ?>
+				</div>
 			</div>
 		</div>
 	</body>
