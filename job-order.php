64e999ee69221f08f6790aaa15a1aa24ad4773c5
carolino024 <carolinokevin@gmail.com>
2015-11-23 00:41:31 +0800

    - add edit button on job order

--- job-order.php
+++ job-order.php
@@ -38,8 +38,8 @@
                         <div class="actionBtns">
                             <button type="button" id="newjoborderbtn" class="btn btn-info" data-toggle="modal" 
                                 href="#joModal"><i class="fa fa-plus fa-fw"></i> New Job Order </button>
-                            <!-- <button type="button" id="editbtn" class="btn btn-info" data-toggle="modal">
-                                <i class="fa fa-pencil-square-o fa-fw"></i> Edit </button> -->
+                             <button type="button" id="editbtn" class="btn btn-info" data-toggle="modal">
+                                <i class="fa fa-pencil-square-o fa-fw"></i> Edit </button> 
                             <button type="button" id="updatebtn" class="btn btn-info" data-toggle="modal">
                                 <i class="fa fa-pencil-square-o fa-fw"></i> Update </button>
                             <div class="btn-group">
