59ca4e3d04c9210014e2842c4c168e6da56bf811
carolino024 <carolinokevin@gmail.com>
2015-11-23 20:08:30 +0800

    - added duplicate checks if a new item is added

--- includes/data-processors/addnewsupply.php
+++ includes/data-processors/addnewsupply.php
@@ -5,57 +5,76 @@
     if(isset($_POST["save"])=="save") {
 
         $modelno = $_POST["modelno"];
-        $inventName = $_POST["inventoryname"];
+        $inventName = $_POST["invtname"];
         $inventSize = $_POST["inventorysize"];
         $inventPrice = $_POST["inventoryprice"];
         $quantity = $_POST["inventoryquantity"];
         $reorderlevel = $_POST["reorderlevel"];
 
-        $sqlcheckmodel = "SELECT modelno from models where modelno = '$modelno'";
-        $s = $conn->query($sqlcheckmodel);
-        $ss = $s->fetch_assoc(); 
-        if ($ss==null){
-            $sql1 =  "INSERT INTO models (modelno)
-                  VALUES ('$modelno')";
+                $sqlcheckmodel = "SELECT modelno from models where modelno = '$modelno'";
+                $s = $conn->query($sqlcheckmodel);
+                $ss = $s->fetch_assoc(); 
+                if ($ss==null){
+                    $sql1 =  "INSERT INTO models (modelno)
+                          VALUES ('$modelno')";
 
-            $stmt = $conn->prepare($sql1);
-            $stmt ->execute();
+                    $stmt = $conn->prepare($sql1);
+                    $stmt ->execute();
 
-                  $modelID = 0;
+                          $modelID = 0;
 
-            $result = $conn->query("SELECT modelid from models where modelno = '$modelno'");
+                    $result = $conn->query("SELECT modelid from models where modelno = '$modelno'");
 
-            while ($row=mysqli_fetch_row($result))
-                {
-                    $modelID = $row[0];
-                }
-            
-            //echo "<script>alert('$modelID')</script>";
+                    while ($row=mysqli_fetch_row($result))
+                        {
+                            $modelID = $row[0];
+                        }
+                    
+                    //echo "<script>alert('$modelID')</script>";
+                            $sqlcheckduplicates = "SELECT * from inventory 
+                                    WHERE inventoryname = '$inventName' && inventorysize = '$inventSize' && modelid = '$modelID' "; //check duplicates
 
-            $sql2 =  "INSERT INTO inventory (inventoryname, inventorysize, inventoryprice, inventoryquantity, 
-                            modelid, reorderlevel)
-                  VALUES ('$inventName', '$inventSize', '$inventPrice', '$quantity', '$modelID', '$reorderlevel')";
+                            $qq = $conn->query($sqlcheckduplicates);
+                            $qqq = $qq->fetch_assoc();
+                            if($qqq=null){
+                            $sql2 =  "INSERT INTO inventory (inventoryname, inventorysize, inventoryprice, inventoryquantity, 
+                                            modelid, reorderlevel)
+                                  VALUES ('$inventName', '$inventSize', '$inventPrice', '$quantity', '$modelID', '$reorderlevel')";
 
-            $stmt1 = $conn->prepare($sql2);
-            $stmt1 ->execute();
+                            $stmt1 = $conn->prepare($sql2);
+                            $stmt1 ->execute();
+                            }else{
+                                echo "<script>alert('ERROR: ITEM - $inventName , SIZE - $inventSize , MODEL - $modelno is currently in database')</script>";
+                            }
+                        }
+                        else{
+                            $result1 = $conn->query("SELECT modelid from models where modelno = '$modelno'");
 
-        }else{
-            $result1 = $conn->query("SELECT modelid from models where modelno = '$modelno'");
+                            $modelID = 0;
 
-            $modelID = 0;
+                            while ($row=mysqli_fetch_row($result1))
+                                 {
+                                   $modelID = $row[0];
+                                 }
+                            $sqlcheckduplicates = "SELECT * from inventory 
+                                    WHERE inventoryname = '$inventName' && inventorysize = '$inventSize' && modelid = '$modelID' "; //check duplicates
 
-            while ($row=mysqli_fetch_row($result1))
-                 {
-                   $modelID = $row[0];
-                 }
+                            $qq = $conn->query($sqlcheckduplicates);
+                            $qqq = $qq->fetch_assoc();
+                            if($qqq=null){
+                            $sql2 =  "INSERT INTO inventory (inventoryname, inventorysize, inventoryprice, inventoryquantity, 
+                                            modelid, reorderlevel)
+                                  VALUES ('$inventName', '$inventSize', '$inventPrice', '$quantity', '$modelID', '$reorderlevel')";
 
-            $sql2 =  "INSERT INTO inventory (inventoryname, inventorysize, inventoryprice, inventoryquantity, 
-                            modelid, reorderlevel)
-                  VALUES ('$inventName', '$inventSize', '$inventPrice', '$quantity', '$modelID', '$reorderlevel')";
+                            $stmt1 = $conn->prepare($sql2);
+                            $stmt1 ->execute();
+                            }else{
+                                echo "<script>alert('ERROR: ITEM - $inventName , SIZE - $inventSize , MODEL - $modelno is currently in database')</script>";
+                            }
+                        }
+                        //echo "<script>alert('ERROR: ITEM - $inventName , SIZE - $inventSize , MODEL - $modelno is currently in database')</script>";
+    
 
-            $stmt1 = $conn->prepare($sql2);
-            $stmt1 ->execute();
-        }
         //-----------------------------UPDATE INVENTORY------------------------------------
 /*        $sql =  "INSERT INTO inventory (inventoryname, inventorysize, inventoryprice, inventoryquantity, 
                             modelid, reorderlevel)
@@ -64,8 +83,9 @@
 
         $stmt->execute(); */
         
-        header('location:../../inventory.php');  
-
+        echo '<script type="text/javascript">'; 
+        echo ' window.location = "../../inventory.php";'; 
+        echo '</script>';
     }
     $conn->close();
 ?>
