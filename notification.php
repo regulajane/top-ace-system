d6c50198fa911316358feab25be15f394a0eef3a
carolino024 <carolinokevin@gmail.com>
2015-11-24 01:05:29 +0800

    - fixed notif page

--- notification.php
+++ notification.php
@@ -7,6 +7,7 @@
 	        if(!isset($_SESSION["username"])) {
 	            header('Location: index.php?loggedout=true');}
 	    ?>
+	    <script src="js/notification-script.js"></script>
 		<title>Notification</title>
 	</head>
 	<body>
@@ -14,9 +15,20 @@
 		<div class="pagecontainer">
 	    	<br>
 			<div class="container">
+
 				<h4>Notifications</h4>
 				<hr style="margin-bottom: 70px">
-				<!-- Inventory Buttons -->
+				<table  id="notificationTable" class="table table-condensed table-hover table-striped">
+				<thead>
+					<tr>
+						<th data-column-id="inventoryname">Item Name</th>
+						<th data-column-id="modelno" >Model No</th>
+						<th data-column-id="inventorysize" >Size</th>
+						<th data-column-id="notificationdetails">Reason</th>
+					</tr>
+					</thead>  
+				</table>
+
 	            <?php include 'includes/footer.php'; ?>
 			</div> 	
 		</div>
