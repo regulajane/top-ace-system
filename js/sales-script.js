76607d1b62bc81d4c8a18e6cdf89da74cf0e2c6a
carolino024 <carolinokevin@gmail.com>
2015-12-22 23:34:58 +0800

    - fixed graph

--- js/sales-script.js
+++ js/sales-script.js
@@ -1,7 +1,47 @@
+var totalJan = 0;
+var totalFeb = 0;
+var totalMar = 0;
+var totalApr = 0;
+var totalMay = 0;
+var totalJun = 0;
+var totalJul = 0;
+var totalAug = 0;
+var totalSep = 0;
+var totalOct = 0;
+var totalNov = 0;
+var totalDec = 0;
+var Year = 0;
+
 window.onload = function () {
 
     navbar();
+    populateGraph(Year);
+
+    $(document).on('change', 'select', function() {
+        
+        var opt = $(this).find('option:selected')[0];
+        // use switch or if/else etc.*/
+        Year = parseInt(opt.innerHTML);
+        //window.location.href = "sales.php";
+        totalJan = 0;
+        totalFeb = 0;
+        totalMar = 0;
+        totalApr = 0;
+        totalMay = 0;
+        totalJun = 0;
+        totalJul = 0;
+        totalAug = 0;
+        totalSep = 0;
+        totalOct = 0;
+        totalNov = 0;
+        totalDec = 0;
+        //Year = 0;
+        populateGraph(Year);
+
+    });
+
     
+
     $("#salesTable").bootgrid({
         ajax: true,
         url: "data-servers/sales-server.php",
@@ -60,7 +100,7 @@ window.onload = function () {
         }
     });
      
-    createChart();
+    //createChart();
 }
 
 function navbar(){
@@ -69,12 +109,20 @@ function navbar(){
     $( items[activeEl] ).addClass('active');
 }
 
-function createChart(){
-var chart = new CanvasJS.Chart("chartContainer",
+
+function createChart(year){
+    //var totalSales1 = parseInt(totalSales1);
+    var headText;
+    if(year == 0){
+        headText = "Total Sales - per month ( Please Select A Year )";
+    }else{
+        headText = "Total Sales - per month ( Year "+year+")";
+    }
+    var chart = new CanvasJS.Chart("chartContainer",
     {
       theme: "theme1",
       title:{
-        text: "Total Sales - per month"
+        text: headText
       },
       animationEnabled: true,
       axisX: {
@@ -92,18 +140,18 @@ var chart = new CanvasJS.Chart("chartContainer",
         type: "line",
         //lineThickness: 3,        
         dataPoints: [
-        { x: new Date(2012, 00, 1), y: 450 },
-        { x: new Date(2012, 01, 1), y: 414},
-        { x: new Date(2012, 02, 1), y: 520, indexLabel: "highest",markerColor: "red", markerType: "triangle"},
-        { x: new Date(2012, 03, 1), y: 460 },
-        { x: new Date(2012, 04, 1), y: 450 },
-        { x: new Date(2012, 05, 1), y: 500 },
-        { x: new Date(2012, 06, 1), y: 480 },
-        { x: new Date(2012, 07, 1), y: 480 },
-        { x: new Date(2012, 08, 1), y: 410 , indexLabel: "lowest",markerColor: "DarkSlateGrey", markerType: "cross"},
-        { x: new Date(2012, 09, 1), y: 500 },
-        { x: new Date(2012, 10, 1), y: 480 },
-        { x: new Date(2012, 11, 1), y: 510 }
+        { x: new Date(year, 00, 1), y: totalJan },
+        { x: new Date(year, 01, 1), y: totalFeb },
+        { x: new Date(year, 02, 1), y: totalMar }, //indexLabel: "highest",markerColor: "red", markerType: "triangle"},
+        { x: new Date(year, 03, 1), y: totalApr },
+        { x: new Date(year, 04, 1), y: totalMay },
+        { x: new Date(year, 05, 1), y: totalJun },
+        { x: new Date(year, 06, 1), y: totalJul },
+        { x: new Date(year, 07, 1), y: totalAug },
+        { x: new Date(year, 08, 1), y: totalSep }, //indexLabel: "lowest",markerColor: "DarkSlateGrey", markerType: "cross"},
+        { x: new Date(year, 09, 1), y: totalOct },
+        { x: new Date(year, 10, 1), y: totalNov },
+        { x: new Date(year, 11, 1), y: totalDec }
         
         ]
       }
@@ -113,4 +161,73 @@ var chart = new CanvasJS.Chart("chartContainer",
     });
 
 chart.render();
+}
+
+function populateGraph(year){
+    $.ajax({
+            type: "POST",
+            url: "includes/data-processors/processSalesGraph.php",
+            success: function(data) {
+
+                year = Year;
+
+                salesData = JSON.parse(data);
+                for(var i = 0; i < salesData.length; i++){
+                    //alert(salesData[i].saledate);
+                    var temp = salesData[i].saledate.split("-");
+                    
+                    if(temp[1] == 1 && temp[0] == year){
+                        totalJan = totalJan + parseInt(salesData[i].total);
+                        //alert( salesData[i].total);
+                    }
+                    if(temp[1] == 2 && temp[0] == year){
+                        totalFeb = totalFeb + parseInt(salesData[i].total);
+                        //alert( salesData[i].total);
+                    }
+                    if(temp[1] == 3 && temp[0] == year){
+                        totalMar = totalMar + parseInt(salesData[i].total);
+                        //alert( salesData[i].total);
+                    }
+                    if(temp[1] == 4 && temp[0] == year){
+                        totalApr = totalApr + parseInt(salesData[i].total);
+                        //alert( salesData[i].total);
+                    }
+                    if(temp[1] == 5 && temp[0] == year){
+                        totalMay = totalMay + parseInt(salesData[i].total);
+                        //alert( salesData[i].total);
+                    }
+                    if(temp[1] == 6 && temp[0] == year){
+                        totalJun = totalJun + parseInt(salesData[i].total);
+                        //alert( salesData[i].total);
+                    }
+                    if(temp[1] == 7 && temp[0] == year){
+                        totalJul = totalJul + parseInt(salesData[i].total);
+                        //alert( salesData[i].total);
+                    }
+                    if(temp[1] == 8 && temp[0] == year){
+                        totalAug = totalAug + parseInt(salesData[i].total);
+                        //alert( salesData[i].total);
+                    }
+                    if(temp[1] == 9 && temp[0] == year){
+                        totalSep = totalSep + parseInt(salesData[i].total);
+                        //alert( salesData[i].total);
+                    }
+                    if(temp[1] == 10 && temp[0] == year){
+                        totalOct = totalOct + parseInt(salesData[i].total);
+                        //alert( salesData[i].total);
+                    }
+                    if(temp[1] == 11 && temp[0] == year){
+                        totalNov = totalNov + parseInt(salesData[i].total);
+                        //alert( salesData[i].total);
+                    }
+                    if(temp[1] == 12 && temp[0] == year){
+                        totalDec = totalDec + parseInt(salesData[i].total);
+                        //alert( salesData[i].total);
+                    } 
+
+
+                }
+                createChart(year);
+            }
+        }); 
 }
